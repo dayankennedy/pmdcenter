f59a9a6a94f022a71cac9b50d8c12e5b2f668d82
dayankennedy <dayankennedyspiid@gmail.com>
2023-04-10 16:30:38 +0300

    gitingore file added

--- PMDCENTER/settings.py
+++ PMDCENTER/settings.py
@@ -13,7 +13,6 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 from pathlib import Path
 import os
 import dj_database_url
-from decouple import  config
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -141,5 +140,3 @@ MEDIA_ROOT=os.path.join(BASE_DIR, '/media/')
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 # Configure Django App for Heroku.
-import django_on_heroku
-django_on_heroku.settings(locals())
